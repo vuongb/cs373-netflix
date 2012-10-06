341af5c62c291b05377cabe1887596cf5d33136f
Bryan Vuong <vuong.bryan@gmail.com>
2012-10-06 01:52:09 -0500

    Added "Average Customer Rating" cache
    Haven't tested yet

---
+++ Netflix.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# ---------------------------
+# Name  : Ryan J. Prater
+#       : Bryan Vuong
+# EID   : rp22566
+#       : bkv85
+# CSID  : rprater
+#       :
+# CS373 - Downing - Project #3 - Netflix
+# ---------------------------
+
+
+# -------------
+# pfd_solve
+# -------------
+
+def Netflix_solve (r, w) :
+
