463a5a66c9bd2ed6c7f00cb0f3d050d05afd2f30
Ryan Prater <ryanprater@utexas.edu>
2012-10-10 01:57:31 -0500

    Played around with RMSE weights

--- Netflix.py
+++ Netflix.py
@@ -42,9 +42,50 @@ def read_avg_customer_cache(input, size):
     return customer_rating_cache
 
 
+
+# Weights
+# (customer:movie) - before extremity testing
+    #  1 : 1 -> 1.1565250495
+    #  2 : 1 -> 1.47431192534
+    #  3 : 1 -> 2.86086310726
+    #  4 : 1 -> 4.40117897681
+    #  1 : 2 -> 1.44662779423
+    #  1 : 3 -> 2.78770284902
+    #  1 : 4 -> 4.28639893013
+    #  0.25 : 1.00 -> 2.01165881019
+    #  0.50 : 1.00 -> 1.70445882173
+    #  0.75 : 1.00 -> 1.40371046471
+    #  0.85 : 1.00 -> 1.27685076585
+    #  0.93 : 1.00 -> 1.20643843618
+    #  0.97 : 1.00 -> 1.17101711143
+    #  1.00 : 0.25 -> 1.98429445923
+    #  1.00 : 0.75 -> 1.39795340259
+    #  1.00 : 0.93 -> 1.20626670786
+    #  1.00 : 0.97 -> 1.16705817031
+# Extremity margins (1:1) weight
+    # 0.25 -> 1.15592131358
+    # 0.50 -> 1.15598683409
+    # 0.75 -> 1.15686153674
 def compute_estimated_rating(avg_customer_rating, avg_movie_rating):
     if avg_customer_rating != 0 :
-        return ((avg_customer_rating*1.0) + avg_movie_rating)/2     # Average the avg_cust_rating and avg_movie_rating (formatted to float)
+
+        # Cast to floats
+        avg_customer_rating = float(avg_customer_rating)
+        avg_movie_rating    = float(avg_movie_rating)
+
+        # Weights
+        customer_rating_weight  = 1.0
+        movie_rating_weight     = 1.0
+
+        # Extremities
+        if avg_customer_rating < 2 or avg_customer_rating > 4:
+            customer_rating_weight += 0.10   # Extremity margin
+        if avg_movie_rating < 2 or avg_movie_rating > 4:
+            movie_rating_weight += 0.10      # Extremity margin
+
+        estimated_rating = ( (avg_customer_rating*customer_rating_weight) + (avg_movie_rating*movie_rating_weight) ) / 2    # Average the avg_cust_rating and avg_movie_rating (considering weights)
+
+        return estimated_rating
     else :
         return avg_movie_rating                                     # If no average customer rating, then just give movie recommendation
 
